cfbeb3a366dbdd2a8f657f06264890a0933dd65e
Eric Ahlberg <eahlberg@gmail.com>
2025-02-02 08:54:48 +0100

    Improve tests

--- test/Main.hs
+++ test/Main.hs
@@ -17,15 +17,18 @@ dummyTime = UTCTime (fromGregorian 2019 10 1) 0
 jan1 :: DiffTime -> UTCTime
 jan1 = UTCTime (fromGregorian 2020 1 1)
 
+jan2 :: DiffTime -> UTCTime
+jan2 = UTCTime (fromGregorian 2020 1 2)
+
 main :: IO ()
 main = hspec $ do
   describe "taskText" $ do
-    it "should return data for a stopped timer" $ do
+    it "should return data for stopped timers" $ do
       let timer =
             Timer
               { timerId = 2,
                 timerStartTime = jan1 0,
-                timerStopTime = Just $ jan1 100,
+                timerStopTime = Just $ jan1 60,
                 timerCreatedAt = dummyTime,
                 timerUpdatedAt = Nothing,
                 timerDescription = Just "stopped timer"
@@ -34,25 +37,37 @@ main = hspec $ do
       let task =
             Task
               { taskId = 1,
-                taskName = "foo-bar",
+                taskName = "task name",
                 taskCreatedAt = jan1 0,
                 taskUpdatedAt = Nothing,
                 taskPeriods = Set.fromList [timer]
               }
 
-      let now = jan1 200
+      let now = dummyTime
       let actual = PrettyPrint.tasksText now [task]
 
-      actual `shouldSatisfy` isInfixOf "foo-bar"
+      actual `shouldSatisfy` isInfixOf "task name"
+
       actual `shouldSatisfy` isInfixOf "2020-01-01 00:00"
       actual `shouldSatisfy` isInfixOf "2020-01-01 00:01"
       actual `shouldSatisfy` isInfixOf "stopped timer"
 
-    it "should return data for a running timer" $ do
-      let timer =
+      actual `shouldSatisfy` isInfixOf "Total time: 1min"
+
+    it "should return data for stopped and running timers" $ do
+      let stoppedTimer =
             Timer
               { timerId = 2,
                 timerStartTime = jan1 0,
+                timerStopTime = Just $ jan1 120,
+                timerCreatedAt = dummyTime,
+                timerUpdatedAt = Nothing,
+                timerDescription = Just "stopped timer"
+              }
+      let runningTimer =
+            Timer
+              { timerId = 2,
+                timerStartTime = jan2 0,
                 timerStopTime = Nothing,
                 timerCreatedAt = dummyTime,
                 timerUpdatedAt = Nothing,
@@ -62,19 +77,26 @@ main = hspec $ do
       let task =
             Task
               { taskId = 1,
-                taskName = "foo-bar",
+                taskName = "task name",
                 taskCreatedAt = jan1 0,
                 taskUpdatedAt = Nothing,
-                taskPeriods = Set.fromList [timer]
+                taskPeriods = Set.fromList [runningTimer, stoppedTimer]
               }
 
-      let now = dummyTime
+      let now = jan2 60
       let actual = PrettyPrint.tasksText now [task]
 
-      actual `shouldSatisfy` isInfixOf "foo-bar"
+      actual `shouldSatisfy` isInfixOf "task name"
+
       actual `shouldSatisfy` isInfixOf "2020-01-01 00:00"
+      actual `shouldSatisfy` isInfixOf "2020-01-01 00:02"
+      actual `shouldSatisfy` isInfixOf "stopped timer"
+
+      actual `shouldSatisfy` isInfixOf "2020-01-02 00:00"
       actual `shouldSatisfy` isInfixOf "RUNNING"
       actual `shouldSatisfy` isInfixOf "running timer"
+
+      actual `shouldSatisfy` isInfixOf "Total time: 3min"
 
   describe "totalTime" $ do
     it "should return sum of start and stop if the timer has stopped" $ do
